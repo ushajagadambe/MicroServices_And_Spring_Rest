3bcfd1ae53983cb87547c9aae3683d0416837167
Usha Jagadambe <jagadambeusha@gmail.com>
2022-02-18 18:49:25 +0530

    spring Actuactor project added to change base path

---
+++ springActuactor/src/main/java/com/gais/springActuactor/SpringActuactorApplication.java
@@ -0,0 +1,13 @@
+package com.gais.springActuactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringActuactorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringActuactorApplication.class, args);
+	}
+
+}
---
+++ springActuactor/src/test/java/com/gais/springActuactor/SpringActuactorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gais.springActuactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringActuactorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
